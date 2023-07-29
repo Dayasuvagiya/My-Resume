54b8f20ba466060b41f5cf13194294b36df0b78d
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-07-29 19:05:22 +0200

    Fix bug in github information

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -36,19 +36,21 @@ function repoInformationHTML(repos) {
             </div>`;
 }
 
-
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
-    if(!username) {
-        $('#gh-user-data').html(`<h2>Please enter a Github username</h2>`)
-        return ;
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
     }
-    
+
     $("#gh-user-data").html(
         `<div id="loader">
-            <img src="assets/css/loader.gif" alt="loading..."/>
+            <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
+
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
@@ -70,3 +72,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
